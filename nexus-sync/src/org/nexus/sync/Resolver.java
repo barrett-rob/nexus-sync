71d2d6fc9e75d7c2dd5cbbe8f7eff97c40479d4b
Rob Barrett <rob.barrett@au.abb.com>
2015-03-20 13:42:20 +1100

    wip

---
+++ nexus-sync/src/org/nexus/sync/Resolver.java
@@ -0,0 +1,15 @@
+package org.nexus.sync;
+
+import java.util.List;
+
+public class Resolver {
+
+	public Resolver(List<String> versions) {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public void resolve() {
+		
+	}
+
+}
--- nexus-sync/src/org/nexus/sync/Sync.java
+++ nexus-sync/src/org/nexus/sync/Sync.java
@@ -1,10 +1,22 @@
 package org.nexus.sync;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.List;
+import java.util.Properties;
+
 public class Sync {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) throws FileNotFoundException, IOException {
+		// load config
+		Properties ps = new Properties();
+		ps.load(new FileInputStream(new File("nexus-sync.properties")));
+		// scan nexus
+		List<String> versions = new Scanner(ps).scan();
+		// resolve
+		new Resolver(versions).resolve();
 	}
 
 }
